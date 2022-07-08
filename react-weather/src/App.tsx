c532d6abd037c2d66a626cb59263cc93d5dc846f
Dimdim28 <wwwdimanes1@gmail.com>
2022-07-08 17:10:23 +0300

    Adding a header

--- react-weather/src/App.tsx
+++ react-weather/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-
+import React from "react";
+//import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home/Home";
+import MonthStatistics from "./pages/MonthStatistics/MonthStatistics";
+import Header from "./shared/header/Header";
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/month-statistics" element={<MonthStatistics />} />
+      </Routes>
     </div>
   );
 }
---
+++ react-weather/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import s from "./Home.module.scss";
+type Props = {};
+
+const Home = (props: Props) => {
+  return <div className={s.home}>Home</div>;
+};
+export default Home;
