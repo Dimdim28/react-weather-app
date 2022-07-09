5fe1ddf5a50a498d803ab0836a5d3cd347c1f60f
Dimdim28 <wwwdimanes1@gmail.com>
2022-07-09 09:13:23 +0300

    Adding thisDay info block

---
+++ react-weather/src/pages/MonthStatistics/components/ThisDayInfo/ThisDayInfo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Select from "react-select";
+import s from "./ThisDayInfo.module.scss";
+type Props = {};
+
+const ThisDayInfo = (props: Props) => {
+  return <header className={s.header}></header>;
+};
+
+export default ThisDayInfo;
