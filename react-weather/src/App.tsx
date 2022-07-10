56b6cec274a1a5cca6e3fdfd5c12583d35ba75b6
Dimdim28 <wwwdimanes1@gmail.com>
2022-07-10 09:09:06 +0300

    Adding a popup

--- react-weather/src/App.tsx
+++ react-weather/src/App.tsx
@@ -4,14 +4,19 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home/Home";
 import MonthStatistics from "./pages/MonthStatistics/MonthStatistics";
 import Header from "./shared/header/Header";
+import Popup from "./shared/Popup/Popup";
+
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/month-statistics" element={<MonthStatistics />} />
-      </Routes>
+    <div className="global-container">
+      <Popup />
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/month-statistics" element={<MonthStatistics />} />
+        </Routes>
+      </div>
     </div>
   );
 }
